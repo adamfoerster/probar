dad6eb574ca91e7280d3c61ea824ce29a07eb41e
Adam Foerster <adam.foerster@baritecnologia.com.br>
2020-03-16 10:37:13 -0300

    logic moved from page to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,99 @@
-import React, { useContext } from 'react';
-import { RankingContext } from '../contexts/RankingContext';
+import React, { useContext, useEffect, useState } from "react";
+import { RankingContext } from "../contexts/RankingContext";
+import firebase, { db } from "../firebase.js";
+import { getUserFromFirebase } from "../helpers";
 
 const Navbar = () => {
-  const { state } = useContext(RankingContext);
+  const { state, dispatch } = useContext(RankingContext);
+  const [savingPlayer, isSavingPlayer] = useState(false);
+
+  const setLogin = u => {
+    const user = getUserFromFirebase(u);
+    dispatch({ type: "SET_USER", user });
+    fetchPlayers();
+  };
+
+  useEffect(() => {
+    listenAuthChanges();
+    fetchPlayers();
+    checkFirstLogin();
+  });
+
+  const listenAuthChanges = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user && !state.user) {
+        setLogin(user);
+      }
+    });
+  };
+
+  const checkFirstLogin = async () => {
+    if (
+      state.playersFetched &&
+      state.user &&
+      state.user.email &&
+      !savingPlayer &&
+      !state.players.find(p => p.email === state.user.email)
+    ) {
+      isSavingPlayer(true);
+      db()
+        .collection("probar-players")
+        .add({ ...state.user, score: 0, ranking: 10000 })
+        .then(() => console.log("first login, adding you to the database"))
+        .catch(error => console.log("ERROR", error));
+    }
+  };
+
+  const fetchPlayers = () => {
+    if (!state.playersFetched) {
+      db()
+        .collection("probar-players")
+        .get()
+        .then(snap => {
+          let players = [];
+          snap.forEach(doc => {
+            players.push(doc.data());
+          });
+          dispatch({ type: "SET_PLAYERS", players });
+          if (savingPlayer) {
+            isSavingPlayer(false);
+          }
+        })
+        .catch(error => console.log("ERROR", error));
+    }
+  };
+
+  const login = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithRedirect(provider)
+      .then(user => {
+        if (user) {
+          setLogin(user);
+        }
+      })
+      .catch(error => console.log(error));
+  };
+
+  const logout = () => {
+    firebase.auth().signOut();
+    dispatch({ type: "LOGOUT" });
+  };
+
   return (
     <div className="navbar">
+      {state.user ? (
+        <div className="right">
+          {state.user.displayName} <button onClick={logout}>Sair</button>
+        </div>
+      ) : (
+        <button onClick={login}>Login</button>
+      )}
       <h1>Pro Bari Ranking</h1>
       <p>Atualmente temos {state.players.length} jogadores no ranking.</p>
     </div>
   );
-}
- 
+};
+
 export default Navbar;
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,99 +1,20 @@
-import React, { useContext, useEffect, useState } from 'react';
-import firebase, { db } from '../firebase.js';
-import { getUserFromFirebase } from '../helpers';
-import { RankingContext } from '../contexts/RankingContext';
-import Ranking from './Ranking';
-import GameReport from './GameReport';
-import Navbar from './Navbar';
+import React, { useContext } from "react";
+import { RankingContext } from "../contexts/RankingContext";
+import Ranking from "./Ranking";
+import GameReport from "./GameReport";
+import Navbar from "./Navbar";
 
 function Page() {
-  const { state, dispatch } = useContext(RankingContext);
-  const [savingPlayer, isSavingPlayer] = useState(false);
-
-  useEffect(() => {
-    listenAuthChanges();
-    fetchPlayers();
-    checkFirstLogin();
-  });
-
-  const listenAuthChanges = () => {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user && !state.user) {
-        setLogin(user);
-      }
-    });
-  };
-
-  const fetchPlayers = () => {
-    if (!state.playersFetched) {
-      db()
-        .collection('probar-players')
-        .get()
-        .then(snap => {
-          let players = [];
-          snap.forEach(doc => {
-            players.push(doc.data());
-          });
-          dispatch({ type: 'SET_PLAYERS', players });
-          if (savingPlayer) {
-            isSavingPlayer(false);
-          }
-        })
-        .catch(error => console.log('ERROR', error));
-    }
-  };
-
-  const checkFirstLogin = async () => {
-    if (
-      state.playersFetched &&
-      state.user &&
-      state.user.email &&
-      !savingPlayer &&
-      !state.players.find(p => p.email === state.user.email)
-      ) {
-      isSavingPlayer(true);
-      db()
-        .collection('probar-players')
-        .add({ ...state.user, score: 0, ranking: 10000 })
-        .then(() => console.log('first login, adding you to the database'))
-        .catch(error => console.log('ERROR', error));
-    }
-  };
-
-  const setLogin = u => {
-    const user = getUserFromFirebase(u);
-    dispatch({ type: 'SET_USER', user });
-    fetchPlayers();
-  };
-
-  const login = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase
-      .auth()
-      .signInWithRedirect(provider)
-      .then(user => {
-        if (user) {
-          setLogin(user);
-        }
-      })
-      .catch(error => console.log(error));
-  };
-
-  const logout = () => {
-    firebase.auth().signOut();
-    dispatch({type: 'LOGOUT'});
-  };
+  const { state } = useContext(RankingContext);
 
   return !!state.user ? (
     <div className="page">
-      <button onClick={logout}>Sair</button>
       <Navbar />
       <GameReport />
       <Ranking />
     </div>
   ) : (
     <div>
-      <button onClick={login}>Login</button>
       <Navbar />
       <Ranking />
     </div>
